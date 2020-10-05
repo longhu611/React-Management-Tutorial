1d5c241e98aacf32b018b650b53c9857eefb7014
zhenqing.piao <zhenqing.piao@honshenol.com>
2020-10-05 23:51:14 +0800

    Add Get Customers Rest Api Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -9,19 +9,39 @@ import { withStyles } from "@material-ui/core/styles"
 
 const styles = theme => ({
     table: {
-        marginTop: theme.spacing.unit * 10,
+        marginTop: theme.spacing(5),
         minWidth: 1080
     }
 })
 
 class Customer extends React.Component {
+
+    state = {
+        customers: []
+    }
+
+    componentDidMount(){
+        this.callApi().then(res => this.setState({customers: res}))
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body;
+    }
+
     render() {
         const { classes } = this.props
+        const { customers } = this.state
+        console.log(customers)
         return (
             <Paper>
              <Table className={classes.table}>
                  <TableHead>
                  <TableRow>
+                     <TableCell>
+                         번호
+                     </TableCell>
                      <TableCell>
                          이름
                      </TableCell>
@@ -37,20 +57,27 @@ class Customer extends React.Component {
                  </TableRow>
                  </TableHead>
                  <TableBody>
-                 <TableRow>
-                     <TableCell>
-                         홍길동
-                     </TableCell>
-                     <TableCell>
-                         남
-                     </TableCell>
-                     <TableCell>
-                         19900116
-                     </TableCell>
-                     <TableCell>
-                         학생
-                     </TableCell>
-                 </TableRow>
+                     {
+                         customers.map(c => (
+                            <TableRow key={c.id}>
+                                <TableCell>
+                                    {c.id}
+                                </TableCell>
+                                <TableCell>
+                                    {c.name}
+                                </TableCell>
+                                <TableCell>
+                                    {c.sex}
+                                </TableCell>
+                                <TableCell>
+                                    {c.birth}
+                                </TableCell>
+                                <TableCell>
+                                    {c.job}
+                                </TableCell>
+                        </TableRow>
+                         ))
+                     }
                  </TableBody>
             </Table>
             </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            "id": 1,
+            "name": "홍길동",
+            "sex": "남",
+            "birth": "900116",
+            "job": "학생"
+        },
+        {
+            "id": 2,
+            "name": "이순신",
+            "sex": "남",
+            "birth": "160116",
+            "job": "장군"
+        },
+        {
+            "id": 3,
+            "name": "신사임당",
+            "sex": "여",
+            "birth": "850116",
+            "job": "5만원"
+        }
+    ]);
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
